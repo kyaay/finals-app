df96e585c3078caa04373282e98060c91d79b4a7
KHIARA DANIELLE T RUBIA <khiara.rubia@usjr.edu.ph>
2022-12-13 03:08:07 +0800

    updated files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import Navbar from "./Navbar";
-//things to install in react
-// npm install sass
-// npm install @mui/icons-material
-// npm install @emotion/styled
-// npm install @emotion/react
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
     </div>
-  );
-}
+)}
 
 export default App;
---
+++ src/Navbar.test.js
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe("Check if there is a nav bar", () => {
+    it("has a logo", () => {
+      render(<App />);
+      const displayLogo = document.querySelector("img");
+      expect(displayLogo.src).toContain("logo");
+    });
+
+    //this test cannot search yet, only input
+    // it("has a search bar", () => {
+    //   render(<App />);
+    //   const search = screen.getByLabelText("search");
+    //   expect(search).toBeInTheDocument();
+    // });
+
+    // it("has a profile button", () => {
+    //   const profile_btn = screen.getByTestId("profile-btn");
+    //   expect(profile_btn).toBeInTheDocument();
+    // });
+
+    // it("has an add to cart button", () => {
+    //   const add_cart = screen.getByTestId("add-to-cart");
+    //   expect(add_cart).toBeInTheDocument();
+    // });
+});
