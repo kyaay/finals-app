12e4f13f96aa2c47d0d4470a3d4fc933c9895300
Denverth Jhay <denverthjhay.larida.21@usjr.edu.ph>
2022-12-13 06:19:44 +0800

    Adjusted Navbar and Added API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import Navbar from "./Navbar";
+import React, { useState, useEffect } from 'react';
+import ProductList from "./ProducList";
+
 //things to install in react
 // npm install sass
-// npm install @mui/icons-material
-// npm install @emotion/styled
-// npm install @emotion/react
 
-function App() {
+const App = () => {
+  const [products,setProducts] = useState([ ]);
+
+  const getproductRequest = async () => {
+    const url = 'https://dummyjson.com/products/category/laptops';
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    
+    setProducts(responseJson.Search);
+  }
+
+  useEffect(() => {
+    getproductRequest();
+  },[]);
+
+
   return (
     <div className="App">
+      <div>
       <Navbar/>
+      </div>
+      <div>
+        <ProductList products = {products}/>
+      </div>     
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -2,22 +2,23 @@ import React from 'react';
 import './Styles/Navbar.scss'
 
 
-
-
 function Navbar(){
     return(
     <div>    
+        <link href="https://fonts.googleapis.com/css2?family=Rubik+80s+Fade&display=swap" rel="stylesheet"></link>
         <div className='navbar'>
-            <h1>Techno</h1>
-                <div className='inputdiv'>
+            <div className='logo'>
+                <h1>TECHNO</h1>
+            </div>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+                <div className='inputBox'>
                     <input type="text"placeholder='Search'/>
                 </div>
             <div className='Links'>
-                <a href=''>Home</a>
-                <a href=''>Home</a>
-                <a href=''>Home</a>
-                <a href=''>Home</a>   
-              
+                <a href='home'><span class="material-symbols-outlined size-30">home</span></a>
+                <a href='contact-us'><span class="material-symbols-outlined size-30">contact_page</span></a>
+                <a href='profile'><span class="material-symbols-outlined size-30">account_circle</span></a>
+                <a href='cart'><span class="material-symbols-outlined size-30">shopping_cart</span></a>                 
             </div>    
     
         </div>
