f7e64df7ce3eb595ab78638b26eb848c5db8a5bf
Denverth Jhay <denverthjhay.larida.21@usjr.edu.ph>
2022-12-15 22:50:14 +0800

    addurlforphonesAPI

--- src/ProductList.js
+++ src/ProductList.js
@@ -33,8 +33,11 @@ const ProductList = ({products}) => {
     )
     /* 
             Jerick mao ni ang sample gikan sa API na link sa mga laptop producs
-            'https://dummyjson.com/products/category/laptops'
+           Laptops: 'https://dummyjson.com/products/category/laptops'
+           Phones: https://dummyjson.com/products/search?q=phone
 
+
+    Laptops
     {"id":6
     "title":"MacBook Pro",
     "description":"MacBook Pro 2021 with mini-LED display may launch between September, November",
@@ -46,6 +49,20 @@ const ProductList = ({products}) => {
     "https://i.dummyjson.com/data/products/6/2.jpg",
     "https://i.dummyjson.com/data/products/6/3.png",
     "https://i.dummyjson.com/data/products/6/4.jpg"]}
+
+    Phones
+    {"products":
+    [{"id":1,
+    "title":"iPhone 9",
+    "description":"An apple mobile which is nothing like apple",
+    "price":549,
+    "discountPercentage":12.96,
+    "rating":4.69,
+    "stock":94,
+    "brand":"Apple",
+    "category":"smartphones",
+    "thumbnail":"https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+    "images":["https://i.dummyjson.com/data/products/1/1.jpg","https://i.dummyjson.com/data/products/1/2.jpg","https://i.dummyjson.com/data/products/1/3.jpg","https://i.dummyjson.com/data/products/1/4.jpg","https://i.dummyjson.com/data/products/1/thumbnail.jpg"]},
     */
 }
 
