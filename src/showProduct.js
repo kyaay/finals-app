b3372c6d39b4502cad4f144e491d8b652172a4ab
Denverth Jhay <denverthjhay.larida.21@usjr.edu.ph>
2022-12-19 04:04:55 +0800

    adjusted buy design

--- src/showProduct.js
+++ src/showProduct.js
@@ -1,42 +1,36 @@
-// import { useState } from "react";
 import './Styles/showProduct.scss';
 
 const ShowProduct = () =>{
 
     return(
         <div className="singleProd">
-            <div className="row">
-            <div className="column">
-            <div className="productImg">
-                <image src="https://i.dummyjson.com/data/products/6/thumbnail.png"></image>
-            </div>
-            </div>
-            <div className="column">
-            <div className="productDetails">
-                <h1>MacBook Pro</h1>
-                <p>MacBook Pro 2021 with mini-LED display may launch between September, November</p>
-                <hr class="solid"></hr>
-                <p><b>Rating: </b>4.57%</p>
-                <p><b>Stock: </b>83</p>
-                <p><b>Brand: </b>Apple</p>
-                <p><b>Category: </b>laptop</p>
-            </div>
-            </div>
-            <div className="column">
-            <div className="checkoutBt">
-                <h1>₱ 1,749.00</h1>
-                <div className='quantity'>
-                    <label>Quantity:</label>
-                    <input type="number" min="1" max="100" id='quantity'></input>
+            <div className='singleProd_container'>
+                <div className="row1">
+                    <div className="column">
+                        <div className="productImg">
+                            <img src="https://i.dummyjson.com/data/products/6/thumbnail.png"></img>
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div className="productDetails">
+                            <h1>MacBook Pro</h1>
+                            <p>MacBook Pro 2021 with mini-LED display may launch between September, November</p>
+                            <hr class="solid"></hr>
+                            <p><b>Rating: </b>4.57%</p>
+                            <p><b>Stock: </b>83</p>
+                            <p><b>Brand: </b>Apple</p>
+                            <p><b>Category: </b>laptop</p>
+                            <hr class="solid"></hr>
+                            <h2>₱ 1,749.00</h2>
+                            <div className='quantity'>
+                                <label>Quantity: </label>
+                                <input type="number" min="1" max="100" id='quantity'></input>
+                            </div>
+                            <button>Checkout</button>
+                        </div>
+                    </div>
                 </div>
-                <button>Checkout</button>
-
             </div>
-
-            </div>
-
-            </div>
-          
         </div>
     );
 }
