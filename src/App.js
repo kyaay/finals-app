0aee9d8fce8396fba134572bf15daafdf1914774
Jerick Cumayas <jerickroyce.cumayas.20@usjr.edu.ph>
2022-12-21 11:41:17 +0800

    addtocart-changes

--- src/App.js
+++ src/App.js
@@ -33,9 +33,9 @@ const App = () => {
   }
 
   function addToCart(value){
-    cartList.push(value);
-    console.log(cartList);
-    setCartList(cartList);
+    const newList = [...cartList];
+    newList.push(value);
+    setCartList(newList);
     localStorage.setItem('listItems', JSON.stringify(cartList));
   }
 
